0d74ebe9de0d9eff9069af5e6d9316569eefa09b
cinarefebedir <146859461+cinarefebedir@users.noreply.github.com>
2023-10-10 16:43:09 +0000

    input ve len yazdım

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello world')
+# print(
+#     input("Merhaba, adın nedir?")
+# )
+# print("Merhaba " + input("Merhaba,adın nedir?")) 
+# print(len(input("Merhaba adın nedir?")))
+İsim = input("Merhaba adın nedir?")
+print(İsim)
+print("Merhaba" + İsim )
