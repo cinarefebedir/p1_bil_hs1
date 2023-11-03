55fd8db2ab9305e7534413b40493015ce0d67842
cinarefebedir <146859461+cinarefebedir@users.noreply.github.com>
2023-11-03 16:42:39 +0000

    akış diyagramı 10.

--- main.py
+++ main.py
@@ -1,8 +1,230 @@
-# print(
-#     input("Merhaba, adın nedir?")
-# )
-# print("Merhaba " + input("Merhaba,adın nedir?")) 
-# print(len(input("Merhaba adın nedir?")))
-İsim = input("Merhaba adın nedir?")
-print(İsim)
-print("Merhaba" + İsim )
+# # # # # # # # print(
+# # # # # # # #     input("Merhaba, adın nedir?")
+# # # # # # # # )
+# # # # # # # # print("Merhaba " + input("Merhaba,adın nedir?")) 
+# # # # # # # # print(len(input("Merhaba adın nedir?")))
+# # # # # # # # İsim = input("Merhaba adın nedir?")
+# # # # # # # # print(İsim)
+# # # # # # # # print("Merhaba" + İsim ) 
+# # # # # # # # İsim = "Çınar"
+# # # # # # # # print(İsim)
+# # # # # # # # İsim = "efe"
+# # # # # # # # print(İsim)
+# # # # # # # # print(len(input("Merhaba, adın nedir?")))
+# # # # # # # # isim = input("Merhaba adın nedir?")
+# # # # # # # # uzunluk = len(isim)
+# # # # # # # # print(uzunluk)
+# # # # # # # # Sayi1 = input("1nci sayıyı giriniz: ")
+# # # # # # # # Sayi2 = input("2nci sayıyı giriniz: ")
+# # # # # # # # Sayi3 = Sayi1
+# # # # # # # # Sayi1 = Sayi2
+# # # # # # # # Sayi2 = Sayi3
+# # # # # # # # print("Sayı1 : " + Sayi1)
+# # # # # # # # print("Sayı2 : " + Sayi2)
+# # # # # # # # enSonGidilenSehirAdi = input("Hangi Şehire gittiniz en son?")
+# # # # # # # # evcilHayvanAdi = input("Evcil hayvanınızın adı nedir?")
+# # # # # # # # print("Grup Adı: "+" "+ enSonGidilenSehirAdi + " " + evcilHayvanAdi)
+# # # # # # # # sayi1 = input("1inci Sayıyı Giriniz: ")
+# # # # # # # # sayi2 = input("2nci Sayıyı Giriniz: ")
+# # # # # # # # toplam = int(sayi1) + int(sayi2)
+# # # # # # # # print("Toplam =" + str(toplam))
+# # # # # # # # kisaKenar = int(input("Kısa kenarı giriniz: "))
+# # # # # # # # uzunKenar = int(input("Uzun kenarı giriniz: "))
+# # # # # # # # cevre = (kisaKenar + uzunKenar) * 2
+# # # # # # # # alan = kisaKenar * uzunKenar
+# # # # # # # # print("Dikdörtgenin Çevresi: " + str(cevre))
+# # # # # # # # print("Dikdörtgenin Alanı: " + str(alan))
+# # # # # # # # yaricap = int(input("Yarıçapı giriniz: "))
+# # # # # # # # yukseklik = int(input("Yüksekliği giriniz: "))
+# # # # # # # # pisayisi = 3.14
+# # # # # # # # tabanAlani = (pisayisi * yaricap * yaricap)
+# # # # # # # # tabanCevresi = 2 * pisayisi * yaricap
+# # # # # # # # yanalAlan = yukseklik * tabanCevresi
+# # # # # # # # toplamAlan = yanalAlan + (2 * tabanAlani)
+# # # # # # # # hacim = tabanAlani * yukseklik
+# # # # # # # # print("Silindirin Hacmi: " + (str(hacim)))
+# # # # # # # # print("Silindir Alanı: " + (str(toplamAlan)))
+# # # # # # # # yaricap = int(input("Yarıçapı giriniz: "))
+# # # # # # # # pisayisi = 3.14
+# # # # # # # # cevre = 2 * pisayisi * yaricap
+# # # # # # # # hacim = pisayisi * yaricap * yaricap
+# # # # # # # # print("Dairenin Hacmi: " + str(hacim))
+# # # # # # # # print("Dairenin Çevresi: " + str(cevre))
+# # # # # # # # print(len(str(1234567890)))
+# # # # # # # # print("Merhaba"[0])
+# # # # # # # # print(123 + 456)
+# # # # # # # # print(123_456_789)
+# # # # # # # # print(3.12345)
+# # # # # # # # print(True)
+# # # # # # # # print(False)
+# # # # # # # # isimUzunluk = len(input("Merhaba, Adın Ne?"))
+# # # # # # # # isimUzunluk_Str = str(isimUzunluk)
+# # # # # # # # print("İsimde " + isimUzunluk_Str + " karakter var.")
+# # # # # # # # fl = float(3.242)
+# # # # # # # # print(type(fl))
+# # # # # # # # intt = int(195)
+# # # # # # # # print(type(intt))
+# # # # # # # # st = str(878787)
+# # # # # # # # print(type(st))
+# # # # # # # # print(100 + float(15.34))
+# # # # # # # # print(str(122) + str(324))
+# # # # # # # # ikihanelisayi = str(input("İki haneli bir sayı giriniz!"))
+# # # # # # # # toplam = int(ikihanelisayi[0]) + int(ikihanelisayi[1])
+# # # # # # # # print("Toplam: " + str(toplam))
+# # # # # # # print(2 * 4 + 6 / 3 - 2)
+# # # # # # # boy = float(input("Boyunuzu giriniz (m): "))
+# # # # # # # kilo = float(input("Kilonuzu giriniz (kg):"))
+# # # # # # # ortaaa = int(kilo/boy**2)
+# # # # # # # print("Vücut Kitle İndeksiniz: " + str(ortaaa))
+# # # # # # # print(8/3)
+# # # # # # # print(type(8/3))
+# # # # # # # print(8//3)
+# # # # # # # print(type(8//3))
+# # # # # # # result = 8/4
+# # # # # # # result /= 2
+# # # # # # # print(result)
+# # # # # # # sayi = 5
+# # # # # # # sayi +=1 
+# # # # # # # print(sayi)
+# # # # # # # sayi = sayi+1
+# # # # # # # print(sayi)
+# # # # # # # print("Sayı = " +str(sayi))
+# # # # # # # print(f"Sayı = {sayi}")
+# # # # # # yas = input("Kaç yaşındasın?")
+# # # # # # yas_int = int(yas)
+# # # # # # ay = yas_int * 12
+# # # # # # hafta = yas_int * 52
+# # # # # # gun = yas_int * 365
+# # # # # # print(f"{ay} aylıksın")
+# # # # # # print(f"{hafta} haftalıksın")
+# # # # # # print(f"{gun} günlüksün")
+# # # # # # print("Bahşiş hesaplayıcıya hoş geldiniz!")
+# # # # # # hesap = float(input("Hesap ne geldi?"))
+# # # # # # kisiSayisi = int(input("Kaç kişisiniz?"))
+# # # # # # oran = float(input("Bahşiş oranı ne olsun?"))
+# # # # # # toplam = hesap +(hesap*oran/100)
+# # # # # # kisiBasi = toplam /kisiSayisi
+# # # # # # # print(f"Kişi başı ödemeniz gereken tutar = {kisiBasi} TL")
+# # # # # print("Lunaparka Hoş geldiniz!")
+# # # # # print("Bilet fiyatı yeişkin 100 TL. 1 yaşından küçükler 59 TL")
+# # # # # boy = int(input("Lütfen boyunuzu giriniz!(cm)"))
+# # # # # yas = int(input("Lütfen yaşınızı giriniz."))
+# # # # # biletfiyati = 0
+# # # # # if boy > 80 :
+# # # # #     if(yas < 1):
+# # # # #         biletfiyati = 59
+# # # # #     else:
+# # # # #         biletfiyati = 100
+# # # # #     print(f"Çarpışan Arabalara Binebilirsiniz! Fiyat {biletfiyati} TL")
+# # # # # elif boy > 80 and boy < 120:
+# # # # #     if(yas < 1):
+# # # # #         biletfiyati = 59
+# # # # #     else:
+# # # # #         biletfiyati = 100
+# # # # #     print(f"Lunaparkta hız trenine binebilirsiniz! Fiyat {biletfiyati} TL")
+# # # # # elif boy > 120 and boy < 140:
+# # # # #     if(yas < 1):
+# # # # #         biletfiyati = 59
+# # # # #     else:
+# # # # #         biletfiyati = 100
+# # # # #     print(f"Gondola binebilirsiniz! Fiyat {biletfiyati} TL")
+# # # # # elif boy > 140:
+# # # # #     if(yas < 1):
+# # # # #         biletfiyati = 59
+# # # # #     else:
+# # # # #         biletfiyati = 100
+# # # # #     print(f"Kamikazeye binebilirsiniz! Fiyat {biletfiyati} TL")
+# # # # # else:  
+# # # # #     print("Lunaparkta herhangi bir araca binmek için boyunuz yetmiyor!")
+# # # # sayi = int(input("Lütfen tek mi çift mi olduğunu öğrenmek için bir sayı giriniz"))
+# # # # kalan = sayi % 2
+# # # # if kalan == 0:
+# # # #     print("Girdiğiniz sayı bir çift sayıdır!")
+# # # # else:
+# # # #     # print("Girdiğiniz sayı tek sayıdır!")
+# boy = float(input("Boyunuzu giriniz (m): "))
+# kilo = float(input("Kilonuzu giriniz (kg):"))
+
+# vki = int(kilo/boy**2)
+# if vki < 18.5:
+#      print(f"Vücüt Kitle İndeksiniz: {vki} , Yemek ye!")
+# elif vki > 18.5 and vki < 25:
+#      print(f"Vücüt Kitle İndeksiniz: {vki} , Devamke!")
+# elif vki > 25 and vki < 30:
+#      print(f"Vücüt Kitle İndeksiniz: {vki} , Yürü lan!")
+# elif vki > 30 and vki < 35:
+#      print(f"Vücüt Kitle İndeksiniz: {vki} , Obez!")
+# elif vki > 35:
+#      print(f"Vücut Kitle İndeksiniz: {vki} , Toprağa Dokunun!")
+# # yil = int(input("Hangi yılı kontrol etmek istiyorsunuz?"))
+# # if yil % 4 ==0:
+# #     if yil % 100 == 0:
+# #         if yil % 400 == 0:
+# #             print(f"{yil} bir artık yıldır!.")
+# #         else:
+# #             print(f"{yil} bir artık yıl değildir!.")
+# #     else:
+# #         print(f"{yil} bir artık yıl değildir!.")
+# # else:
+# #     print(f"{yil} bir artık yıl değildir!.")
+# print("Lunaparka Hoş Geldiniz!")
+# boy = int(input("Boyunuz kaç cm?"))
+# ucret = 0 
+# if boy >= 120:
+#     print("rcostera binebilirsiniz!")
+#     yas = int(input("Kaç yaşındasınız?"))
+#     if yas < 12:
+#         ucret = 5
+#     elif yas <= 18:
+#         ucret = 10
+#     else:
+#         ucret = 20
+#     fotografIstiyor = input("Fotoğraf Çektirmek istiyor musunuz? E veya H")
+#     if fotografIstiyor == "E":
+#         ucret += 3
+#     print(f"Toplam ödeme: {ucret}")
+# else:
+#     print("Üzgünüm, boyunuz rcostera yeterli değil!")
+# boy = input("Doeda Pizzaya Hoş Geldiniz! Hangi Boy Pizza İsterseniz? K veya O veya B =>")
+# ekstraSos = input("Ekstra Sos İster Misiniz? E veya H =>")
+# ekstraPeynir = input("Ekstra Peynir İster misiniz? E veya H =>")
+# toplam = 0
+# if boy == "K":
+#     toplam += 15
+# elif boy == "0":
+#     toplam += 20
+# elif boy == "B":
+#     toplam += 25
+
+# if ekstraSos == "E":
+#     if boy == "K":
+#         toplam += 2
+#     else:
+#         toplam += 5
+# if ekstraPeynir == "E":
+#     toplam += 3
+
+# print(f"Toplam Ödeme = {toplam}")
+print("Sevgi Hesaplayıcıya HG")
+isim1 = input("Adınız?")
+isim2 = input("Onun adı?")
+
+isimler = (isim1 + isim2 ). lower()
+g = isimler.count("g")
+e = isimler.count("e")
+r = isimler.count("r")
+ç = isimler.count("ç")
+e = isimler.count("e")
+k = isimler.count("k")
+a = isimler.count("a")
+ş = isimler.count("ş")
+k = isimler.count("k")
+
+toplam = g + e + r + ç + e + k + a + ş + k
+
+if(toplam < 10 and toplam > 90):
+    print(f"Sevgi puanınız: {toplam}, kola mentos gibisiniz")
+elif(toplam > 40 and toplam < 50):
+    print(f"Sevgi Puanınız: {toplam}, beraber iyisiniz!")
+else:
+    print(f"Sevgi Puanınız: {toplam}")
